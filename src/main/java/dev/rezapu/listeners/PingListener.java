688feabb1e2aa4297dd226a05fe201db52ec1b92
Rezapu Obed <obed.ubaidillah@gmail.com>
2024-02-15 07:19:37 +0700

    feat: add ping command

---
+++ src/main/java/dev/rezapu/listeners/PingListener.java
@@ -0,0 +1,35 @@
+package dev.rezapu.listeners;
+
+import dev.rezapu.commands.PingCommand;
+import dev.rezapu.enums.CommandAccessLevel;
+import dev.rezapu.utils.CommandsUtil;
+import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
+import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
+import org.jetbrains.annotations.NotNull;
+
+public class PingListener extends BaseListener {
+    public PingListener(){
+        PingCommand pingCommand = new PingCommand(CommandAccessLevel.PUBLIC, "Pings the server");
+        CommandsUtil.addCommand(pingCommand, true, true);
+    }
+
+    @Override
+    public void onMessageReceived(@NotNull MessageReceivedEvent event){
+        try{
+            if (event.getMessage().getContentRaw().equals(".ping")){
+                PingCommand pingCommand = CommandsUtil.getCommand(PingCommand.class);
+                if(pingCommand!=null) pingCommand.action(event);
+            }
+        }catch (Exception e){
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public void onSlashCommandInteraction(SlashCommandInteractionEvent event) {
+        if (event.getName().equals("ping")){
+            PingCommand pingCommand = CommandsUtil.getCommand(PingCommand.class);
+            if(pingCommand!=null) pingCommand.action(event);
+        }
+    }
+}
