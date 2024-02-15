f04fa7d0b57aee067c69ea09778e1ce34f966fed
Rezapu Obed <obed.ubaidillah@gmail.com>
2024-02-15 19:22:33 +0700

    feat: add invalid usage

--- src/main/java/dev/rezapu/commands/BaseCommand.java
+++ src/main/java/dev/rezapu/commands/BaseCommand.java
@@ -2,10 +2,12 @@ package dev.rezapu.commands;
 
 import dev.rezapu.enums.CommandAccessLevel;
 import dev.rezapu.enums.CommandPatternType;
+import dev.rezapu.utils.CommandsUtil;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NonNull;
 import lombok.Setter;
+import org.jetbrains.annotations.NotNull;
 
 @AllArgsConstructor
 public abstract class BaseCommand {
@@ -13,4 +15,10 @@ public abstract class BaseCommand {
     protected final CommandAccessLevel accessLevel;
     @Getter @Setter @NonNull
     protected String description;
+    @Getter @NonNull
+    protected final CommandPatternType[] commandPatternTypes;
+
+    protected boolean isUsageValid(String message){
+        return CommandsUtil.match(message, commandPatternTypes);
+    }
 }
