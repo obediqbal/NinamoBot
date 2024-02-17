4e623bd9b1ab2f588b703c5ab5a7cad90414197a
Rezapu Obed <obed.ubaidillah@gmail.com>
2024-02-17 16:04:42 +0700

    feat: add getAllSortedByPoints method

--- src/main/java/dev/rezapu/dao/MemberDAO.java
+++ src/main/java/dev/rezapu/dao/MemberDAO.java
@@ -5,6 +5,8 @@ import dev.rezapu.utils.HibernateUtil;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 
+import java.util.List;
+
 public class MemberDAO extends BaseDAO<Member>{
     public Member getByDiscordId(String discord_id){
         try(Session session = sessionFactory.openSession()){
@@ -21,6 +23,14 @@ public class MemberDAO extends BaseDAO<Member>{
             return session.createQuery("FROM Member WHERE ign = :ign", Member.class)
                     .setParameter("discord_id", ign)
                     .uniqueResult();
+        }
+    }
+
+    public List<Member> getAllSortedByPoints(){
+        try(Session session = sessionFactory.openSession()){
+            session.beginTransaction();
+            return session.createQuery("FROM Member ORDER BY point DESC", Member.class)
+                    .getResultList();
         }
     }
 }
