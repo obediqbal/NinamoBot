04428ffea04c5535a24784d141e8c2068b80def6
HilmiAnarya <hilmidrone@gmail.com>
2024-02-28 11:02:00 +0700

    role hilmi to mod

--- src/main/java/dev/rezapu/utils/CommandsUtil.java
+++ src/main/java/dev/rezapu/utils/CommandsUtil.java
@@ -8,11 +8,14 @@ import dev.rezapu.enums.CommandAccessLevel;
 import dev.rezapu.enums.CommandPatternType;
 import dev.rezapu.exceptions.UnauthorizedException;
 import net.dv8tion.jda.api.Permission;
+import net.dv8tion.jda.api.entities.Guild;
+import net.dv8tion.jda.api.entities.Invite;
 import net.dv8tion.jda.api.entities.Member;
-import net.dv8tion.jda.api.entities.Role;
 import net.dv8tion.jda.api.events.interaction.command.GenericCommandInteractionEvent;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
+import net.dv8tion.jda.api.requests.Route;
 
+import javax.management.relation.Role;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -50,21 +53,21 @@ public class CommandsUtil {
     private static <T extends BaseCommand> T getCommand(Class<T> clazz, Member member, Class<? extends ActionableCommands> clazzAct) throws UnauthorizedException, InstantiationException{
         if(clazzAct.isAssignableFrom(clazz)){
             T command = (T) commandsUtil.commands.get(clazz);
-            if (CommandsUtil.isAuthorized(Objects.requireNonNull(member), command.getAccessLevel()))
+            if (CommandsUtil.isAuthorized(Objects.requireNonNull(member), member.getGuild(), command.getAccessLevel()))
                 return command;
             throw new UnauthorizedException();
         }
         throw new InstantiationException();
     }
 
-    private static boolean isAuthorized(Member member, CommandAccessLevel commandAccessLevel){
+    private static boolean isAuthorized(Member member, Guild guild , CommandAccessLevel commandAccessLevel){
         if(member.getUser().isBot()) return false;
 
 
 
         CommandAccessLevel memberLevel;
         if(member.isOwner() || member.hasPermission(Permission.ADMINISTRATOR)) memberLevel = CommandAccessLevel.ADMIN;
-        else if (member.getUser().getId().equals("446471447481810964")) memberLevel = commandAccessLevel.MOD;
+        else if (member.getGuild().getRoleById("1205192109745766432") != null) memberLevel = commandAccessLevel.MOD;
         else memberLevel = CommandAccessLevel.MEMBER;
 
         return memberLevel.getLevel() >= commandAccessLevel.getLevel();
